5a1470ff88c4801c68cddad76e2b79f858e7b88e
Enzo-belardo <vincenzobelardo10@gmail.com>
2023-03-16 16:55:25 +0100

    edit: fillables, add: method getRouteKeyName into class models user

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,9 +19,12 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'surname',
+        'slug',
         'email',
         'password',
     ];
+
 
     /**
      * The attributes that should be hidden for serialization.
@@ -41,4 +44,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
 }

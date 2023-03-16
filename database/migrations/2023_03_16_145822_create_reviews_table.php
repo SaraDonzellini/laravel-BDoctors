8554111cbe75362583d594ace489e7d31e8e4f0c
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2023-03-16 16:18:52 +0100

    Create table 'reviews'

---
+++ database/migrations/2023_03_16_145822_create_reviews_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned();
+            $table->string('name', 50);
+            $table->string('surname', 50);
+            $table->text('text');
+            $table->tinyInteger('vote')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+};

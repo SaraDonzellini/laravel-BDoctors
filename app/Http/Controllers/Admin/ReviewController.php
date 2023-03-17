78f87792901d3790a295ddd94eda8644742e5bd0
ManuelBravaccini <manuelbravaccini2000@gmail.com>
2023-03-17 16:46:31 +0100

    Add CRUD and route to reviews

---
+++ app/Http/Controllers/Admin/ReviewController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Review;
+use Illuminate\Http\Request;
+
+class ReviewController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public $validationRules = [
+        'name' => 'required|string|min:3|max:255',
+        'surname' => 'required|string|min:3|max:255',
+        'text' => 'required|min:10',
+        'vote' => 'required|integer',
+    ];
+
+    public function index()
+    {
+        $reviews = Review::paginate(10);
+        return view('admin.reviews.index', compact('reviews'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.reviews.create', ['review' => new Review()]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate($this->validationRules);
+
+        $newReview = new Review();
+        $newReview->fill($data);
+        $newReview->save();
+
+        return redirect()->route('admin.reviews.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return view('admin.reviews.show', compact('review'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return view('admin.reviews.edit',  compact('review'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Review $review)
+    {
+        $data = $request->validate($this->validationRules);
+
+        $review->update($data);
+
+        return redirect()->route('admin.reviews.show', compact('review'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Review $review)
+    {
+        $review->delete();
+
+        return redirect()->route('admin.reviews.index')->with('message', "La recensione  \"$review->name\" è stata eliminata")->with('message_class', 'danger');
+    }
+}

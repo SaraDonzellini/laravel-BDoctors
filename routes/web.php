395005b791dee08095eb95fa7c6e7106a9d42057
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2023-03-17 16:04:50 +0100

    Create route for resource 'message'

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\MessageController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,6 +17,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::middleware(['auth', 'verified'])->prefix('admin')->name('admin.')->group(function () {
+    Route::resource('messages', MessageController::class);
 });
 
 Route::get('/dashboard', function () {

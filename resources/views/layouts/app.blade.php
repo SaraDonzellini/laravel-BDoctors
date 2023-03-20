f8e3571890e82fa91975e485952f30299330f13b
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2023-03-20 16:27:57 +0100

    Little fix, in navbar into 'app' layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,7 +43,7 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
                         <li class="nav-item">
-                            <a class="nav-link me-3 {{ (Route::currentRouteName() == 'dashboard') ? 'fw-bold' : ''  }}" href="{{ route('admin.dashboard') }}">{{ __('Home') }}</a>
+                            <a class="nav-link me-3 {{ (Route::currentRouteName() == 'admin.dashboard') ? 'fw-bold' : ''  }}" href="{{ route('admin.dashboard') }}">{{ __('Home') }}</a>
                         </li>
 
                         <li class="nav-item">

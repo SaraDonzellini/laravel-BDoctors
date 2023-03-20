7f07f3a802a605782e566550a42310629355dc35
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2023-03-20 16:31:47 +0100

    Little fix

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -44,11 +44,11 @@
                         <!-- Authentication Links -->
                         @guest
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            <a class="nav-link {{ (Route::currentRouteName() == 'login') ? 'fw-bold' : ''  }}" href="{{ route('login') }}">{{ __('Login') }}</a>
                         </li>
                         @if (Route::has('register'))
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            <a class="nav-link {{ (Route::currentRouteName() == 'register') ? 'fw-bold' : ''  }}" href="{{ route('register') }}">{{ __('Register') }}</a>
                         </li>
                         @endif
                         @else

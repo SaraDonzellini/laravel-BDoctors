ff5ee230179848340f1557ec4621c59ce6071e96
SaraDonzellini <saradonzie@gmail.com>
2023-03-20 16:19:43 +0100

    edited show 'doctors'

--- resources/views/admin/doctors/show.blade.php
+++ resources/views/admin/doctors/show.blade.php
@@ -9,24 +9,37 @@
                     <h3 class="card-title fw-bold my-3">{{ $doctor->user->name }} {{ $doctor->user->surname }}</h3>
                     <img :src="$doctor->photo" alt="">
                     <img :src="$doctor->cv" alt="">
-                    <h4>Address: {{ $doctor->address }}</h4>
-                    <h4>Phone: {{ $doctor->phone }}</h4>
+                    <h4>Indirizzo: {{ $doctor->address }}</h4>
+                    <h4>Telefono: {{ $doctor->phone }}</h4>
                     <h6>Prestazioni: {{ $doctor->performances }}</h6>
-                    <p>BIO <br> {{ $doctor->bio }}</p>
-                    <p>Visibility: todo toggle</p>
+                    <p>Biografia: </p><p> {{ $doctor->bio }}</p>
+                    <p>Visibilià: todo toggle</p>
                 </div>
 
-                <div class="secondary-actions mb-2">
-                    {{-- Edit --}}
-                    <a href="{{ route('admin.doctors.edit', $doctor->id) }}" class="btn btn-warning"><i
-                            class="fa-solid fa-edit"></i></a>
-                    {{-- Delete --}}
+                    <div class="d-flex justify-content-between">
+                        <div class="mb-2 ms-2">
+                            <a href="{{ route('admin.doctors.edit', $doctor->id) }}" class="btn btn-warning">Modifica il tuo profilo</a>
+                        </div>
+                        <div class="mb-2 ">
+                            <a href="{{ route('admin.messages.index', $doctor->id) }}" class="btn btn-info">Leggi i tuoi messaggi</a>
+                        </div>
+                        <div class="mb-2">
+                            <a href="{{ route('admin.reviews.index', $doctor->id) }}" class="btn btn-success">Leggi le tue recensioni</a>
+                        </div>
+                        <div class="mb-2 me-2">
+                            <a href="{{ route('admin.sponsorships.index', $doctor->id) }}" class="btn btn-primary">Scegli una sponsorizzazione</a>
+                        </div>
+                        
+                    </div>
+                    
+
+                    {{-- Delete
                     <form class="d-inline-block form-delete" action="{{ route('admin.doctors.destroy', $doctor->id) }}"
                         method="POST" data-element-name="{{ $doctor->name }}">
                         @csrf
                         @method('DELETE')
                         <button title="Delete" class="btn btn-danger"><i class="fa-solid fa-trash"></i></button>
-                    </form>
+                    </form> --}}
                 </div>
             </div>
         </div>

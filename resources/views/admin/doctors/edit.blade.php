f6481876252e533f6fcd830309afec078efee8b5
GiadaGallitto <gallitto.giada@gmail.com>
2023-03-20 11:06:25 +0100

    fix route edit form

--- resources/views/admin/doctors/edit.blade.php
+++ resources/views/admin/doctors/edit.blade.php
@@ -8,7 +8,7 @@
                     <h6>Inserisci i dati per modificare il tuo profilo</h6>
                 </div>
                 <div class="col-12">
-                    @include('admin.doctors.partials.form',
+                                        @include('admin.doctors.partials.form-create-edit',
                     ['route'=> 'admin.doctors.update', 'method'=> 'PUT', "doctor"=> $doctor])
                 </div>
             </div>

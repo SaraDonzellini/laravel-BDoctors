be1adb3809ca962050665744e47313d66acee16b
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2023-03-21 17:47:50 +0100

    Little fix

--- resources/views/admin/doctors/edit.blade.php
+++ resources/views/admin/doctors/edit.blade.php
@@ -1,11 +1,15 @@
 @extends('layouts.app')
 
+@section('title')
+    Modifica Profilo {{ $doctor->user->name }}!
+@endsection
+
 @section('content')
     <div class="doctor">
-        <div class="container">
+        <div class="container py-4">
             <div class="row justify-content-around">
-                <div class="col-12 text-center mb-3">
-                    <h6>Inserisci i dati per modificare il tuo profilo</h6>
+                <div class="col-12 text-center mb-3 mt-3">
+                    <h3>Inserisci i dati per modificare il tuo profilo</h3>
                 </div>
                 <div class="col-12">
                     @include('admin.doctors.partials.form-create-edit',

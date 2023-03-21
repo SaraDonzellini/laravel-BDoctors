ea2a821d7d870f9a6e0ab391dfc981dea7e4b1e7
ManuelBravaccini <manuelbravaccini2000@gmail.com>
2023-03-21 12:01:50 +0100

    Create doctor form edit

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -3,11 +3,17 @@
 
 <div class="container">
     <h2 class="fs-4 text-secondary my-4">
-        {{ __('Profile') }}
+        {{ __('Modifica profilo') }}
     </h2>
     <div class="card p-4 mb-4 bg-white shadow rounded-lg">
 
         @include('profile.partials.update-profile-information-form')
+
+    </div>
+
+    <div class="card p-4 mb-4 bg-white shadow rounded-lg">
+
+        @include('profile.partials.update-doctor-information-form')
 
     </div>
 

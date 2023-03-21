3db241c2cee8e84bc0b1e414bdfe6684dfc730f1
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2023-03-21 10:44:35 +0100

    Edit redirect into class controller 'RegistredUserController'

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -75,6 +75,6 @@ class RegisteredUserController extends Controller
 
         Auth::login($user);
 
-        return redirect(RouteServiceProvider::HOME);
+        return redirect()->route('admin.doctors.create');
     }
 }

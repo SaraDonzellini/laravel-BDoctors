7de56508950a821ae162ed1a5261e183d4534547
ManuelBravaccini <manuelbravaccini2000@gmail.com>
2023-03-22 12:46:28 +0100

    Create cards for dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,21 +4,61 @@
 
 @section('content')
 
+<div class="container">
+
+    {{-- Welcome message --}}
     @if (Auth::user())
-        <div class="container">
-            <h2 class="fs-4 text-primary my-4">
-                Benvenuto/a {{ $user->name }}
-            </h2>
-
-            <p class="text-info">Sei loggato/a!</p>
-
-            {{-- <a href="" class="btn btn-success btn-sm" type="button">Ultima ora il tuo account</a> --}}
-        </div>
+    <h2 class="fs-4 text-primary my-4">
+        Benvenuto/a {{ $user->name }}
+    </h2>
+    
+    <p class="text-info">Sei loggato/a!</p>
+    
     @else
-        <div class="container">
 
-            <h2 class="mt-5">Non sei loggato, loggati o registrati per accedere al sito.</h2>
-        </div>
+    <h2 class="mt-5">Non sei loggato, loggati o registrati per accedere al sito.</h2>
     @endif
+    
+    <div class="col-12 d-flex gap-4">
 
+    {{-- Doctor's infos --}}
+    <div class="card col-4 p-4 mb-4 bg-white shadow rounded-lg">
+        
+        @include('admin.dashboard-partials.dash-doctor-info')
+        
+    </div>
+    
+    
+    {{-- Doctor's messages --}}
+    <div class="card col-8 p-4 mb-4 bg-white shadow rounded-lg">
+        
+        @include('admin.dashboard-partials.dash-messages')
+        
+    </div>
+    </div>
+
+
+    <div class="col-12 d-flex gap-4">
+    {{-- Doctor's curriculum --}}
+    <div class="card col-4 p-4 mb-4 bg-white shadow rounded-lg">
+        
+        @include('admin.dashboard-partials.dash-curriculum')
+        
+    </div>
+
+    {{-- Doctor's reviews --}}
+    <div class="card col-4 p-4 mb-4 bg-white shadow rounded-lg">
+        
+        @include('admin.dashboard-partials.dash-reviews')
+        
+    </div>
+    
+    {{-- Sponsorships --}}
+    <div class="card col-4 p-4 mb-4 bg-white shadow rounded-lg">
+        
+        @include('admin.dashboard-partials.dash-sponsorships')
+        
+    </div>
+    </div>
+</div>
 @endsection

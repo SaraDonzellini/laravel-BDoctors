e38b431085a10aee2da429cb7f733ce51a97279b
GiadaGallitto <gallitto.giada@gmail.com>
2023-03-23 12:14:25 +0100

    Add reminder to complete the doctor profile

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -17,8 +17,21 @@
     <h2 class="mt-5">Non sei loggato, loggati o registrati per accedere al sito.</h2>
     @endif
 
+    
+    
     <div class="row position-relative">  
-
+        
+        @foreach ($doctor as $key)        
+            @if(empty($key))
+                <div class="w-100 bg-light border border-danger rounded-3 p-3 shadow d-flex justify-content-between align-items-center mb-4">
+                    <span>
+                        Ricordati di completare il tuo profilo dottore!
+                    </span>
+                    <a class="btn btn-dash" href="{{ route('admin.doctors.create', $doctor->id) }}">Completa profilo</a>
+                </div>
+                @break
+            @endif
+        @endforeach
         {{-- Doctor's infos --}}
         <div class="card col-12 col-md-6 col-lg-4 mb-4 bg-white shadow rounded-lg my-card">
             

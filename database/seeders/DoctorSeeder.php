705a2376aa09536b984b0bcad13653a321474b1c
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2023-03-23 10:18:46 +0100

    little fix

--- database/seeders/DoctorSeeder.php
+++ database/seeders/DoctorSeeder.php
@@ -20,7 +20,7 @@ class DoctorSeeder extends Seeder
             [
                 'address' => 'via Veneto, 3',
                 'curriculum' => 'Navy Blue Minimalis Resumes.pdf',
-                'photo' => 'avatar_placeholder.jpg',
+                'photo' => 'imgs/avatar_placeholder.jpg',
                 'phone' => '32499975312',
                 'bio' => 'Laurea in Medicina e Chirurgia, Università degli Studi di Bari Aldo Moro Bari. Votazione di 110/110 e lode con plauso accademico
             Specializzazione in Ginecologia e Ostetricia, Università degli Studi di Bari Aldo Moro, Bari. Votazione 70/70 e lode
@@ -29,7 +29,7 @@ class DoctorSeeder extends Seeder
             [
                 'address' => 'via Roma, 1',
                 'curriculum' => 'Verde Semplice Moderno Curriculum Vitae-1.pdf',
-                'photo' => 'avatar_placeholder.jpg',
+                'photo' => 'imgs/avatar_placeholder.jpg',
                 'phone' => '3332499973',
                 'bio' => 'Ho conseguito la Laurea quinquennale in Psicologia (vecchio ordinamento) nel 2008.
             Ho svolto il tirocinio formativo presso il centro diurno Il Mandala di San Donato Milanese.
@@ -39,14 +39,14 @@ class DoctorSeeder extends Seeder
             [
                 'address' => 'piazza Delle Erbe, 25',
                 'curriculum' => 'Verde Semplice Moderno Curriculum Vitae-2.pdf',
-                'photo' => 'avatar_placeholder.jpg',
+                'photo' => 'imgs/avatar_placeholder.jpg',
                 'phone' => '32499975343',
                 'bio' => 'Medico abilitato alla professione dall’età di 24 anni. Specialista otorinolaringoiatra. Dottore di ricerca in scienze mediche cliniche e sperimentali. Master in Citologia Nasale. Esperta in rinoallergologia e in disturbi respiratori del sonno dell’età adulta e pediatrica.'
             ],
             [
                 'address' => 'via Sicilia, 2',
                 'curriculum' => 'Verde Semplice Moderno Curriculum Vitae-3.pdf',
-                'photo' => 'avatar_placeholder.jpg',
+                'photo' => 'imgs/avatar_placeholder.jpg',
                 'phone' => '32499967312',
                 'bio' => 'Laurea in Medicina e Chirurgia nel 2004.
             Specializzazione in Endocrinologia e Malattia del Ricambio nel 2009.
@@ -56,7 +56,7 @@ class DoctorSeeder extends Seeder
             [
                 'address' => 'via Garibaldi, 5',
                 'curriculum' => 'Verde Semplice Moderno Curriculum Vitae-4.pdf',
-                'photo' => 'avatar_placeholder.jpg',
+                'photo' => 'imgs/avatar_placeholder.jpg',
                 'phone' => '3456789001',
                 'bio' => 'Sono un medico-chirurgo specialista in Urologia.
             Mi sono laureata in Medicina e Chirurgia a Bologna nel 1998 e specializzata con lode nel 2003 sempre a Bologna.
@@ -65,7 +65,7 @@ class DoctorSeeder extends Seeder
             [
                 'address' => 'piazza Verdi, 15',
                 'curriculum' => 'Verde Semplice Moderno Curriculum Vitae-5.pdf',
-                'photo' => 'avatar_placeholder.jpg',
+                'photo' => 'imgs/avatar_placeholder.jpg',
                 'phone' => '3336789543',
                 'bio' => 'Sono un psicologo iscritta all\'Ordine degli Psicologi del Piemonte (n° 9064).
             Mi sono laureata in Psicologia Criminologica e Forense presso l\'Università di Torino e attualmente sono specializzanda in psicoterapia cognitiva-comportamentale presso la CBT Academy di Torino.
@@ -74,7 +74,7 @@ class DoctorSeeder extends Seeder
             [
                 'address' => 'Via Milano, 17',
                 'curriculum' => 'Verde Semplice Moderno Curriculum Vitae-6.pdf',
-                'photo' => 'avatar_placeholder.jpg',
+                'photo' => 'imgs/avatar_placeholder.jpg',
                 'phone' => '3462525369',
                 'bio' => 'Attraverso il colloquio offro uno spazio di ascolto, di riflessione e di comprensione. Ti aiuterò a confrontarti con le richieste interiori che possono nascere in particolari momenti della vita. La finalità è quella di promuovere il benessere nel rapporto con te stesso e con l\'altro (la tua/il tuo partner, la tua famiglia, i tuoi amici, il tuo lavoro).
             L\'emergenza che attualmente viviamo è una situazione stressante che ha messo e mette a dura prova la quotidianità di ognuno, aumentando talvolta situazioni psicologicamente difficili da attraversare. Momenti di ansia, di confusione, di non comprensione sono normali. Attraverso i colloqui ti permetterò di riflettere su strategie di adattamento ad una realtà cambiata, facendoti apprezzare la necessità dell\'evoluzione personale attraverso l\'ascolto interiore.
@@ -83,7 +83,7 @@ class DoctorSeeder extends Seeder
             [
                 'address' => 'Via Chieri, 19',
                 'curriculum' => 'Verde Semplice Moderno Curriculum Vitae.pdf',
-                'photo' => 'avatar_placeholder.jpg',
+                'photo' => 'imgs/avatar_placeholder.jpg',
                 'phone' => '3462675369',
                 'bio' => 'Dopo la laurea in scienze infermieristiche, il tirocinio in ospedale, e una tesi riguardante il bambino diabetico di tipo 1 e lo sport, ho capito che la nutrizione poteva fare la differenza nella prevenzione, nella gestione e nel trattamento di differenti problemi di salute, ma anche in altri ambiti, come quello sportivo.
             Ho deciso così di continuare gli studi di Biologia iniziati dopo il liceo, frequentando il corso di laurea magistrale in Biological Sciences presso l’università di Camerino e laureandomi con 110 e lode. Parallelamente ho frequentato anche il master SANIS con durata biennale spinta dalla mia passione per lo sport, che pratico da sempre.
@@ -92,7 +92,7 @@ class DoctorSeeder extends Seeder
             [
                 'address' => 'Via Mancini, 159',
                 'curriculum' => 'Navy Blue Minimalis Resumes.pdf',
-                'photo' => 'avatar_placeholder.jpg',
+                'photo' => 'imgs/avatar_placeholder.jpg',
                 'phone' => '3462658969',
                 'bio' => 'Sono Dirigente Medico di I livello, in servizio presso l\'Unità Operativa Complessa di Otorinolaringoiatria del Presidio Ospedaliero " R. Dimiccoli".
             Mi sono laureato in Medicina e Chirurgia nel 2013 presso l\'Università di Bari e specializzato in Otorinolaringoiatria e Chirurgia Cervico-Facciale nel 2018 nella medesima Università.

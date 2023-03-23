9951d8f990bbacfcf6b10a56f8a3080060ebd31c
GiadaGallitto <gallitto.giada@gmail.com>
2023-03-23 15:09:44 +0100

    Fix messages and reviews view

--- resources/views/admin/messages/index.blade.php
+++ resources/views/admin/messages/index.blade.php
@@ -10,8 +10,8 @@
             <div class="card mb-3 mt-5 shadow-lg">
                 <div class="card bg-review  p-3 text-start">
                     <div class="text text-light my-4">
-                        <span>Id: {{ $message->id }}</span>
-                        <h3 class="card-title fw-bold my-3">{{ $message->user->name }} {{ $message->user->surname }}</h3>
+                        {{-- <span>Id: {{ $message->id }}</span> --}}
+                        <h3 class="card-title fw-bold my-3">{{ $message->name }} {{ $message->surname }}</h3>
                         <p>{{ $message->text }}</p>
                         <a href="{{ route('admin.messages.show', $message->id) }}" class="btn btn-primary">
                             <i class="fa-solid fa-eye"></i>
--- resources/views/admin/reviews/index.blade.php
+++ resources/views/admin/reviews/index.blade.php
@@ -9,8 +9,8 @@
         <div class="card mb-3 mt-5 shadow-lg">
             <div class="card bg-review  p-3 text-start">
                 <div class="text text-light my-4">
-                    <span>Id: {{ $review->id }}</span>
-                    <h3 class="card-title fw-bold my-3">{{ $review->user->name }} {{ $review->user->surname }}</h3>
+                    <h3 class="card-title fw-bold my-3">{{ $review->name }} {{ $review->surname }}</h3>
+                    <span class="mb-2">Voto: {{ $review->vote }}</span>
                     <p>{{ $review->text }}</p>
                 </div>
             </div>

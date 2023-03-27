32172fba2327f0e329967d16efd688dfea341e09
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2023-03-27 17:38:03 +0200

    Create method 'store' into class controller 'ReviewController'

--- app/Http/Controllers/Api/ReviewController.php
+++ app/Http/Controllers/Api/ReviewController.php
@@ -10,18 +10,26 @@ use Illuminate\Support\Facades\Auth;
 
 class ReviewController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $reviews = Review::all();
         return response()->json([
             'success' => true,
             'response' => $reviews
         ]);
     }
-    // public function show(Doctor $doctor){
-    //     $reviews = Review::with('user', 'doctor')->findOrFail($doctor->id);
-    //     return response()->json([
-    //         'success' => true,
-    //         'response' => $reviews
-    //     ]);
-    // }
+
+
+    public function store(Request $request)
+    {
+        $review = new Review();
+        $review->user_id = $request->userId;
+        $review->name = $request->input('name');
+        $review->surname = $request->input('surname');
+        $review->vote = $request->input('vote');
+        $review->text = $request->input('content');
+        $review->save();
+
+        return response()->json(['success' => true]);
+    }
 }

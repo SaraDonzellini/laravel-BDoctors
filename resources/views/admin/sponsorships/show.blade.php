14767cc0ae7d66606b2f86661d8c746fa0b6bd98
ManuelBravaccini <manuelbravaccini2000@gmail.com>
2023-03-30 11:51:34 +0200

    Imported braintree payment form

--- resources/views/admin/sponsorships/show.blade.php
+++ resources/views/admin/sponsorships/show.blade.php
@@ -18,6 +18,10 @@
                                 {{ $sponsorship->duration }} ore
                             </p>
                         </div>
+                        <script src="https://js.braintreegateway.com/web/dropin/1.36.0/js/dropin.js"></script>
+                        
+                        <div id="dropin-container"></div>
+                        <button id="submit-button" class="button button--small button--green">Purchase</button>
                     </div>
                 </div>
             </div>

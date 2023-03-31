5e82c3dbfd6ea1ce5f232a4e433ab0ff61b02a04
SaraDonzellini <saradonzie@gmail.com>
2023-03-31 14:18:59 +0200

    fixed Show doctor single page

--- resources/views/admin/doctors/show.blade.php
+++ resources/views/admin/doctors/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('title')
-    Dottor {{ $doctor->user->name }}!
+    Dottor {{ $doctor->user->name }}
 @endsection
 
 @section('content')
@@ -12,28 +12,40 @@
     @endif
     <div class="container card shadow-lg mt-5 mb-5">
         <div class="row ">
-            <div class="col-6 ">
+            <div class="col-12  col-sm-12 col-lg-6">
                 <div class="my_header d-flex align-items-center m-auto mb-5 mt-5">
-                    <img class="my_avatar" src="{{ asset("storage/$doctor->photo") }}"
+                    <img class="my_avatar card shadow-lg" src="{{ asset("storage/$doctor->photo") }}"
                         alt="{{ $doctor->user->name }}'s image profile">
                 </div>
             </div>
-            <div class="col-6">
+            <div class="col-12  col-sm-12 col-lg-6">
                 <h1 class="ms-5 text-center fw-bold text-uppercase my_header d-flex align-items-center m-4">
                     {{ $doctor->user->name }} {{ $doctor->user->surname }}</h1>
                 <h5>Specializzazione:</h5>
                 @foreach ($doctor->specializations as $specialization)
-                    <span>{{ $specialization->title }}</span>
+                    <span>| {{ $specialization->title }}</span> |
                 @endforeach
                 <h5 class="mt-2">Prestazione:</h5>
                 <p>{{ $doctor->performance }}</p>
-                <h4>Indirizzo: {{ $doctor->address }}</h4>
-                <h4>Telefono: {{ $doctor->phone }}</h4><br>
+                <div class="my_divbox mt-5 mb-4 ">
+                    <a href="{{ route('admin.messages.index', $doctor->id) }}" class="my_btn">Leggi i tuoi
+                        messaggi</a>
+                    <a href="{{ route('admin.reviews.index', $doctor->id) }}" class="my_btn">Leggi le tue
+                        recensioni</a>
+                </div>
+                <div class="my_divbox mt-5 mb-4 ">
+                    <a href="{{ route('admin.doctors.edit', $doctor->id) }}" class="my_btn">Modifica il tuo
+                        profilo</a>
+                    <a class="sponsorship" href="{{ route('admin.sponsorships.index', $doctor->id) }}">Scegli una
+                        sponsorizzazione</a>
+                </div>
+
 
             </div>
             <div class="row">
                 <div class="col-12 ">
                     <div class="blue-box w-100">
+                        <h2>Visibilità</h2>
                     </div>
                 </div>
 
@@ -41,9 +53,9 @@
             <div class="row">
                 <div class="col-12 ">
                     <div class="my_box text-centered m-5">
-                        <h4>
-                            Visibilità (decidi se renderti visibile o meno ai pazienti)
-                        </h4>
+                        <h5>
+                            (decidi se renderti visibile o meno ai pazienti)
+                        </h5>
                         <form action="{{ route('admin.doctors.toggle', $doctor->id) }}" method="POST">
                             @method('PATCH')
                             @csrf
@@ -58,44 +70,31 @@
             <div class="row">
                 <div class="col-12">
                     <div class="blue-box w-100">
+                        <h2>Informazioni formazione e lavoro</h2>
                     </div>
                 </div>
             </div>
             <div class="row">
-                <div class="col-6 ">
-                    <div class="box ">
-                        <h5 class="m-5">Biografia: </h5>
-                        <p>{{ $doctor->bio }}</p>
+                <div class="col-12  col-sm-12 col-lg-6 ">
+                    <div class="box m-5">
+                        <h5 class="">Biografia: </h5>
+                        <p class="">{{ $doctor->bio }}</p>
+                        <h5>Indirizzo: </h5>
+                        <p>{{ $doctor->address }}</p>
+                        <h5>Telefono: </h5>
+                        <p>{{ $doctor->phone }}</p>
 
                     </div>
                 </div>
-                <div class="col-6 ">
+                <div class="col-12 col-sm-12 col-lg-6 ">
                     <div class="box ">
                         <h5 class="m-5">Curriculum: </h5>
-                        <img class="my_curriculum" src="{{ asset("storage/$doctor->curriculum") }}"
+                        <img class="my_curriculum card" src="{{ asset("storage/$doctor->curriculum") }}"
                             alt="{{ $doctor->user->name }}'s curriculum">
 
                     </div>
                 </div>
             </div>
-
-            <div class="d-flex justify-content-between">
-                <div class="my_divbox mt-5 mb-4 ms-2">
-                    <a href="{{ route('admin.doctors.edit', $doctor->id) }}" class="my_btn">Modifica il tuo
-                        profilo</a>
-                </div>
-                <div class="my_divbox mt-5 mb-4 ">
-                    <a href="{{ route('admin.messages.index', $doctor->id) }}" class="my_btn">Leggi i tuoi
-                        messaggi</a>
-                </div>
-                <div class="my_divbox mt-5 mb-4">
-                    <a href="{{ route('admin.reviews.index', $doctor->id) }}" class="my_btn">Leggi le tue
-                        recensioni</a>
-                </div>
-                <div class="my_divbox mt-5 mb-4 me-2">
-                    <a href="{{ route('admin.sponsorships.index', $doctor->id) }}" class=" my_btn">Scegli una
-                        sponsorizzazione</a>
-                </div>
-
-            </div>
-        @endsection
+        </div>
+    </div>
+    @endsection

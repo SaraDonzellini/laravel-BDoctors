cc30d979ac9f8dfc6c3d5ae5fa50936c62cac586
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2023-03-31 17:38:48 +0200

    fix sponsorship in dashboard user

--- resources/views/admin/dashboard-partials/dash-sponsorships.blade.php
+++ resources/views/admin/dashboard-partials/dash-sponsorships.blade.php
@@ -2,29 +2,33 @@
 namespace App\Models;
 use Illuminate\Support\Facades\Auth;
 
-$sponsorships = Sponsorship::all();
+$user = Auth::user();
+
+$sponsorships = $user->sponsorships;
 
 ?>
 
 <section>
     <div class="d-flex justify-content-center p-4">
         {{-- Route to sponsorhips --}}
-        <a href="{{ route('admin.sponsorships.index', $doctor->id) }}" class="btn-dash position-dash">
+        <a href="#" class="btn-dash position-dash">
             Sponsorizzazioni
         </a>
     </div>
 
     <div>
-        {{-- Sponsorships --}}
-        @foreach ($sponsorships as $sponsorship)
-        <h5>
-            {{ $sponsorship->price }}
+        @if($sponsorships->count() > 0)
+        <h5> 
+            Sponsorizzazione Attiva:
         </h5>
-        <h6>
-            Durata: {{ $sponsorship->duration }} ore
-        </h6>
-        <p>{{ $sponsorship->specifics }}</p>
+        @foreach($sponsorships as $sponsorship)
+            <h4 class="fw-bold">
+                {{ $sponsorship->specifics}}
+            </h4>
         @endforeach
-
+        @else
+            <p>Nessuna sponsorizzazione è stata acquistata.</p>
+            <a href="{{ route('admin.sponsorships.index')}}">Acquistane una ora</a>
+        @endif
     </div>
 </section>

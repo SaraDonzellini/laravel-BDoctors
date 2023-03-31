fa423a36c1efbc97bb7c6414ac7f3c5efc816d3f
SaraDonzellini <saradonzie@gmail.com>
2023-03-31 18:05:29 +0200

    removed message show, adjusted message index

--- resources/views/admin/messages/index.blade.php
+++ resources/views/admin/messages/index.blade.php
@@ -12,10 +12,11 @@
                     <div class="text text-light my-4">
                         {{-- <span>Id: {{ $message->id }}</span> --}}
                         <h3 class="card-title fw-bold my-3">{{ $message->name }} {{ $message->surname }}</h3>
+                        <h4>{{ $message->email }}</h4>
                         <p>{{ $message->text }}</p>
-                        <a href="{{ route('admin.messages.show', $message->id) }}" class="btn btn-primary">
+                        {{-- <a href="{{ route('admin.messages.show', $message->id) }}" class="btn btn-primary">
                             <i class="fa-solid fa-eye"></i>
-                        </a>
+                        </a> --}}
                     </div>
                 </div>
             </div>
